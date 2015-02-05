f7cac7e9998fab5653f3de65fd469c9e394869ad
Dylan Young <dylanyoungmeijer@gmail.com>
2015-02-05 18:56:56 -0400

    Add str manip fncns

--- proxy.c
+++ proxy.c
@@ -23,6 +23,46 @@ void error(const char *msg) {
     exit(1);
 }
 
+/* assert strlen(new) <= strlen(old) */
+char * replace_str(char * source, char * old, char * new)
+{
+    char * start = strstr(source, old);
+    if ( start == NULL || strlen(new) > strlen(old) )
+        return source;
+    char * rest = start+strlen(old);
+    memcpy(start, new, strlen(new));
+    memmove(start+strlen(new),rest,strlen(rest));
+
+    return source;
+}
+
+char * repl_tween_str(char * source, char * start, char * end, char * new)
+{
+    start = strstr(source, start);
+    end = strstr(source, end);
+    if(start == NULL || end == NULL || start >= end)
+        return source;
+    size_t length = end-start;
+    size_t difference = length - strlen(new);
+    if (strlen(new) > length)
+        return source;
+    memcpy(start, new, strlen(new));
+    memmove(start+strlen(new), end, strlen(end));
+    memset(source+strlen(source)-difference, 0, difference);
+    return source;
+}
+
+char * get_tween_str(char * source, char * start, char * end)
+{
+    start = strstr(source, start)+strlen(start);
+    end = strstr(source, end);
+    size_t length = end - start;
+    char * result = malloc((length+1)*sizeof(char));
+    memcpy(result, start, length);
+    memset(result+length+1,0,1);
+    return result;
+}
+
 int proxyserver(char * portstr)
 {
     int sockfd = 0;
